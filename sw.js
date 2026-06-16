b6d98fb920d8c53f3fa1ffabb730d073c621fee5
roozbeh7070 <rkeyhanfar@yahoo.com>
2026-06-16 20:59:42 +0000

    Deploying to gh-pages from @ roozbeh7070/flashboxp@26e776e1ce776a2e2833e64ed2dc06093d6ae3f8 🚀

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'lang-app-v10';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './css/style.css',
+    './js/app.js',
+    './font/Vazirmatn-Regular.ttf',
+    './font/Vazirmatn-Bold.ttf',
+    './doc/logo.png',
+    './doc/icon.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return Promise.allSettled(
+                ASSETS.map(url => cache.add(url).catch(err => console.log('Failed to cache:', url, err)))
+            );
+        })
+    );
+});
+
+// Activate & Cleanup Old Caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Fetch Assets from Cache
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request, { ignoreSearch: true }).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request).catch(() => null);
+        })
+    );
+});
